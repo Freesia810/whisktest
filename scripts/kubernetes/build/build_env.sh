ae4b1edfa3687f66dd9b20e65484ad61ab39a62d
freesia <z1402121306@hotmail.com>
2024-07-31 17:54:31 +0800

    修改：     scripts/kubernetes/build/build_env.sh 	修改：     scripts/kubernetes/build/install_helm.sh

--- scripts/kubernetes/build/build_env.sh
+++ scripts/kubernetes/build/build_env.sh
@@ -3,7 +3,7 @@
 # install docker
 
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -12,7 +12,7 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo chmod 777 /var/run/docker.sock
 
 # install cri-dockerd
